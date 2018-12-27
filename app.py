836ae2779bc3e52ae4bd3a20388a2eb3518cb11b
gmdmgithub <gmika@interia.pl>
2018-12-27 23:31:02 +0100

    port from args

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ from flask_sandbox import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000) # delfault is left but can be change to any other (ofcourse can be change on Apache, Ngnix...)
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p','--port',default=5000)
+    args = parser.parse_args()
+    port = args.port
+    app.run(debug=True, port=port) # delfault is left but can be change to any other (ofcourse can be change on Apache, Ngnix...)
