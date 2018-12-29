c5267be1eea06dac534d917fe1f498b4a9361f49
gmdmgithub <gmika@interia.pl>
2018-12-29 21:03:06 +0100

    amends

--- flask_sandbox/config_app.py
+++ flask_sandbox/config_app.py
@@ -14,6 +14,6 @@ class Config:
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    DEBUG = False
+    DEBUG = os.environ.get("DEBUG")
     TESTING = False
     CSRF_ENABLED = True
